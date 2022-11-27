0504ff53978f05c3d6ebc798e861d829856a01a4
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-11-28 00:58:56 +0900

    feat: add to call for help

---
+++ algorithms/baekjoon/implementation/boj5339.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdin = open("../input.txt", "r")
+
+if __name__ == "__main__":
+    print(r"""     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |""")
