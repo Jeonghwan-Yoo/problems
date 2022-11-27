c3ff675386f963a001214101fe77208d6730bf60
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-11-27 23:47:21 +0900

    feat: add to some marks

---
+++ algorithms/baekjoon/implementation/boj10926.java
@@ -0,0 +1,21 @@
+package dynamic_programming;
+
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj10926 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("./input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String id = br.readLine();
+        sb.append(id).append("??!");
+
+        System.out.println(sb);
+        br.close();
+    }
+
+}
