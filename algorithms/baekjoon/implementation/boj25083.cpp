0105d26752dfdf1e20465a09691aaac6e5cc5035
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-11-27 23:56:56 +0900

    feat: add to print a sprout

---
+++ algorithms/baekjoon/implementation/boj25083.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    freopen("../input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+
+    return 0;
+}
