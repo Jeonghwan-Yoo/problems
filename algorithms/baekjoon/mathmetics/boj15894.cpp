52b65aef1f970b9977b3536133a154d7325f8335
Jeonghwan-Yoo <dwgbjdhks2@gmail.com>
2022-12-09 00:13:50 +0900

    feat: add to math is physical education

---
+++ algorithms/baekjoon/mathmetics/boj15894.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    freopen("../input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long N;
+    cin >> N;
+    cout << (N << 2);
+
+    return 0;
+}
